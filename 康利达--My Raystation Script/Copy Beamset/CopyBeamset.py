8f4075576076818d55b45d9317115924fb3f419f
az7jh2 <50506882+az7jh2@users.noreply.github.com>
2021-01-29 14:08:13 -0500

    Update CopyBeamset.py
    V4版更新：
    1.增加刚性配准功能
    2.增加判断机制
      判断计划和射束集是否一致（测试通过）
      判断要复制beamset是否为SMLC或3D-CRT（测试通过）
      判断plan和from CT是否对应（测试通过）
      判断配准模式下两幅CT是否为同一CT（测试通过）
      判断配准模式下两幅CT是否存在配准（测试通过）
      判断新计划名是否已存在（beamset可同名）（测试通过）
      如果新beamset名字超过16个字符，只取前16个（测试通过）

--- 康利达--My Raystation Script/Copy Beamset/CopyBeamset.py
+++ 康利达--My Raystation Script/Copy Beamset/CopyBeamset.py
@@ -3,80 +3,130 @@
 #Created by Ningshan Li 2015-6-23
 
 from connect import *
+import ctypes
+#-----------------------------------------------------------------
+def Mbox(title, text, style):
+    ctypes.windll.user32.MessageBoxW(0, text, title, style)
 #---------------------------------------------------------------------------------#
-def copybeam(patient,beam_set,exam,isoset):
+def copybeam(patient,beamset,fromexamname,toexamname,mode,newplanname,newbeamsetname):
     #复制所选射束集参数
-    modality = beam_set.Modality  #如Photons
-    plantech = beam_set.PlanGenerationTechnique  #如IMRT
-    '''deliverytech = beam_set.DeliveryTechnique   #如SMLC'''
-    pos = beam_set.PatientPosition   #如HeadFirstSupine
-    linac = beam_set.MachineReference.MachineName  #MachineReference中没有能量
-    fracs = beam_set.FractionationPattern.NumberOfFractions
+    modality = beamset.Modality  #如Photons
+    plantech = beamset.PlanGenerationTechnique  #如IMRT
+    pos = beamset.PatientPosition   #如HeadFirstSupine
+    linac = beamset.MachineReference.MachineName  #MachineReference中没有能量
+    fracs = beamset.FractionationPattern.NumberOfFractions
     if plantech=='Imrt':
         tech='SMLC'
     elif plantech=='Conformal':
         tech='Conformal'
     
     #新建计划
-    patient.AddNewPlan(PlanName='Copiedplan',ExaminationName=exam)
-    plan=patient.TreatmentPlans['Copiedplan']
+    patient.AddNewPlan(PlanName=newplanname,ExaminationName=toexamname)
+    #载入新建计划
+    info=patient.QueryPlanInfo(Filter={'Name':'^{0}$'.format(newplanname)})
+    if len(info)==1:
+        plan=patient.LoadPlan(PlanInfo=info[0])
+    else:
+        raise Exception('No plan named {0} exists for the current patient'.format(newplanname))
     #在新计划中新建空白射束集,治疗技术只能为SMLC或Comformal
-    plan.AddNewBeamSet(Name='Copied',ExaminationName=exam, MachineName = linac, Modality=modality, 
+    plan.AddNewBeamSet(Name=newbeamsetname,ExaminationName=toexamname, MachineName = linac, Modality=modality, 
       TreatmentTechnique = tech, PatientPosition = pos, NumberOfFractions=fracs)
 
-    infos = plan.QueryBeamSetInfo(Filter={'Name':'Copied'})
-    new = plan.LoadBeamSet(BeamSetInfo=infos[0])
+    info = plan.QueryBeamSetInfo(Filter={'Name':'^{0}$'.format(newbeamsetname)})
+    if len(info)==1:
+        newbeamset = plan.LoadBeamSet(BeamSetInfo=info[0])
+    else:
+        raise Exception('No beamset named {0} exists for the current plan'.format(newbeamsetname))
     
     #复制射束,无法用len来获取原计划射野个数
     i=0 #记录射野的num，用于赋值
-    for beam in beam_set.Beams:
+    for beam in beamset.Beams:
         #射野中心和能量
-        if isoset=='Copy from prototype':
+        if mode=='Copy from prototype':
             xpos = beam.PatientToBeamMapping.IsocenterPoint.x
             ypos = beam.PatientToBeamMapping.IsocenterPoint.y
             zpos = beam.PatientToBeamMapping.IsocenterPoint.z
-        elif isoset=='Zeros':
+        elif mode=='Zeros':
             xpos=0
             ypos=0
             zpos=0
+        elif mode=='Rigid Registration':
+            xorgin = beam.PatientToBeamMapping.IsocenterPoint.x
+            yorgin = beam.PatientToBeamMapping.IsocenterPoint.y
+            zorgin = beam.PatientToBeamMapping.IsocenterPoint.z
+            newiso=patient.TransformPointFromExaminationToExaminationUsingTotalTransform(FromExamination=fromexamname,ToExamination=toexamname,
+              Point= {'x':xorgin, 'y':yorgin, 'z':zorgin})
+            xpos=newiso.x
+            ypos=newiso.y
+            zpos=newiso.z
         else:
             raise RuntimeError('The selected mode of setting isocenter is wrong') 
         energy = beam.MachineReference.Energy
         name = beam.Name + "_Copy"
         
-        new.CreatePhotonBeam(Energy=energy, Isocenter = {'x':xpos, 'y':ypos, 'z':zpos}, Name = name, 
+        newbeamset.CreatePhotonBeam(Energy=energy, Isocenter = {'x':xpos, 'y':ypos, 'z':zpos}, Name = name, 
           GantryAngle = beam.GantryAngle, CouchAngle = beam.CouchAngle, CollimatorAngle = beam.InitialCollimatorAngle)
-        new.Beams[i].BeamMU=beam.BeamMU
+        newbeamset.Beams[i].BeamMU=beam.BeamMU
             
         #读取子野
         j=0
         for seg in beam.Segments:
-            new.Beams[i].CreateRectangularField()
-            new.Beams[i].Segments[j].JawPositions =  seg.JawPositions 
-            new.Beams[i].Segments[j].LeafPositions = seg.LeafPositions
-            new.Beams[i].Segments[j].RelativeWeight = seg.RelativeWeight
+            newbeamset.Beams[i].CreateRectangularField()
+            newbeamset.Beams[i].Segments[j].JawPositions =  seg.JawPositions 
+            newbeamset.Beams[i].Segments[j].LeafPositions = seg.LeafPositions
+            newbeamset.Beams[i].Segments[j].RelativeWeight = seg.RelativeWeight
             j+=1
             
         i+=1
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------#
 #主程序
-def main(plan,beamset,fromexam,toexam,mode):
+def main(planname,beamsetname,fromexamname,toexamname,mode,newplanname,newbeamsetname):
     #patient获取为当前患者
     patient=get_current('Patient')
     
-   
-    beam_set=patient.TreatmentPlans[plan].BeamSets[beamset]
-    
-    
+    #判断计划和射束集
+    try:
+        beam_set=patient.TreatmentPlans[planname].BeamSets[beamsetname]
+    except:
+        Mbox('Warning','The selected beamset dose not belong to the selected plan',0)
+        return 0
+    #判断beamset,CRT或者SMLC的技术都是SMLC
+    if not(beam_set.DeliveryTechnique=='SMLC'):
+        Mbox('Warning','The selected beamset is not with the technique of SMLC or CRT',0)
+        return 0
+    #判断plan和fromCT是否一致
+    if not(patient.TreatmentPlans[planname].GetStructureSet().OnExamination.Name==fromexamname):
+        Mbox('Warning','The selected plan dose not correspond to the select from examination',0)
+        return 0
+    #判断配准模式下是否存在配准
+    if mode=='Rigid Registration':
+        if fromexamname==toexamname:
+            Mbox('Warning','The selected from examination is identical to the selected to examination',0)
+            return 0
+        else:
+            if patient.GetTotalTransformForExaminations(FromExamination=fromexamname,ToExamination=toexamname)==None:
+                Mbox('Warning','There is no rigid registration between selected examinations',0)
+                return 0
+    #判断新计划名是否存在
+    existplannames=[]
+    for plan in patient.TreatmentPlans:
+        existplannames.append(plan.Name)
+    if newplanname in existplannames:
+        Mbox('Warning','The inputed name of new plan is already exist',0)
+        return 0
+    #计算新射束名称位数
+    if len(newbeamsetname)>16:
+        newbeamsetname=newbeamsetname[:16]
     #复制射野至指定CT
-    copybeam(patient,beam_set,toexam,mode)
+    copybeam(patient=patient,beamset=beam_set,fromexamname=fromexamname,toexamname=toexamname,mode=mode,newplanname=newplanname,newbeamsetname=newbeamsetname)
     
     #设置网格，计算剂量
-    patient.TreatmentPlans['Copiedplan'].SetDefaultDoseGrid(VoxelSize={ 'x': 0.3, 'y': 0.3, 'z': 0.3 })
-    patient.TreatmentPlans['Copiedplan'].ComputeRoiVoxelVolumes()
-    patient.TreatmentPlans['Copiedplan'].BeamSets['Copied'].ComputeDose(ComputeBeamDoses=True, 
+    patient.TreatmentPlans[newplanname].SetDefaultDoseGrid(VoxelSize={ 'x': 0.3, 'y': 0.3, 'z': 0.3 })
+    patient.TreatmentPlans[newplanname].ComputeRoiVoxelVolumes()
+    patient.TreatmentPlans[newplanname].BeamSets[newbeamsetname].ComputeDose(ComputeBeamDoses=True, 
       DoseAlgorithm="CCDose", ForceRecompute=False)
-
+    return 1
 #-----------------------------------------------------------------------------#
 if __name__=='__main__':
-    main(plan,beamset,fromexam,toexam,mode)
+    r=main(planname='Plan_CT1',beamsetname='Plan_CT1',fromexamname='CT 1',toexamname='CT 1',mode='Zeros',newplanname='test2',newbeamsetname='1')
+    print r
