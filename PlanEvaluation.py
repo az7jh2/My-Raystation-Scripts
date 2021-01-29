9ac32fb90efdbff0ba6bbbe19bbdc8d015c98d8d
az7jh2 <50506882+az7jh2@users.noreply.github.com>
2021-01-29 14:53:39 -0500

    Update PlanEvaluation.py
    V2更新
    1.加入patientlist列表，目前只指定其为空列表，表明只处理当前打开病例
    2.预置的roi名称被删除
    3.为方便修改roi名称和剂量学指标，一个对象加其相应的剂量学指标采用字典结构保存，同时检查函数中也添加对字典类型的处理
      检查功能测试通过
      应用时修改只用在main中修改即可
    4.增加求ROI绝对体积（cc)对应的剂量功能
    5.增加自动寻找外轮廓ROI

--- PlanEvaluation.py
+++ PlanEvaluation.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #剂量统计
+#python中，空为false
 #created by Ningshan Li 2015/7/2
 from connect import *
 from collections import OrderedDict
@@ -22,9 +23,9 @@ def GetAbsoluteVolume(total_dose,RoiName):
     return TotalRoiVolume
 #--------------------------------------------------------------------------------------------------------------#
 #求适形度指数CI
-def GetConformationIndex(total_dose, DoseValue, RoiName):
+def GetConformationIndex(total_dose, DoseValue, RoiName,extername):
     TotalTargetVolume = GetAbsoluteVolume(total_dose, RoiName) #target总体积，float类型
-    TotalDoseVolume = GetAbsoluteDoseVolume(total_dose,DoseValue,"External")
+    TotalDoseVolume = GetAbsoluteDoseVolume(total_dose,DoseValue,extername)
     TargetDoseVolume = GetAbsoluteDoseVolume(total_dose,DoseValue,RoiName)
     return TargetDoseVolume*TargetDoseVolume/(TotalDoseVolume*TotalTargetVolume)
 #------------------------------------------------------------------------------------------------------------#
@@ -57,106 +58,101 @@ def check(gather,objects):
     index=range(len(objects))
     index.reverse()
     for i in index:
-        if not(objects[i] in gather):
-            objects.remove(objects[i])
+        if isinstance(objects[i],str): #列表中的对象为str类型，表明是planlist的名称判断
+            if not(objects[i] in gather):
+                objects.remove(objects[i])
+        elif isinstance(objects[i],dict): #列表中的对象为dict类型，表明是oarlist的名称判断
+            if not(objects[i]['name'] in gather):
+                objects.remove(objects[i])
+#-------------------------------------------------------------------------------------#
+def getdoseindicators(total_dose,ROI,extername):
+    d=OrderedDict()
+    d['roiname']=ROI['name']
+    if ROI['Ds']: # Dose指标不为空
+        Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = ROI['name'], RelativeVolumes = [i/100.0 for i in ROI['Ds']])
+        for i in range(len(ROI['Ds'])):
+            name='D'+str(ROI['Ds'][i])
+            d[name]=Doses[i]
+    if ROI['Vs']: # Volume指标不为空
+        Volumes = total_dose.GetRelativeVolumeAtDoseValues(RoiName = ROI['name'], DoseValues=[i*100.0 for i in ROI['Vs']])
+        for i in range(len(ROI['Vs'])):
+            name='V'+str(ROI['Vs'][i])
+            d[name]=Volumes[i]
+    if ROI['special']: #特殊类型不为空
+        for j in ROI['special']:
+            if cmp(j,'max')==0: #查询max
+                d['DMax'] = total_dose.GetDoseStatistic(RoiName = ROI['name'], DoseType='Max')
+            elif cmp(j,'mean')==0:
+                d['DAve'] = total_dose.GetDoseStatistic(RoiName = ROI['name'], DoseType='Average')
+            elif cmp(j,'HI')==0:
+                try:
+                    d['HI']=(d['D2']-d['D98'])/d['D50']  #如果字典中包含这些数值，就直接用
+                except: #如果没有，就重新计算
+                    tmpdose = total_dose.GetDoseAtRelativeVolumes(RoiName = ROI['name'], RelativeVolumes = [0.02,0.5,0.98])
+                    d['HI']=(tmpdose[0]-tmpdose[2])/tmpdose[1]
+            elif cmp(j,'CI')==0:
+                d['CI']=GetConformationIndex(total_dose, ROI['prescription'], ROI['name'],extername)
+            elif j.find('cc')!=-1: #绝对体积相关的剂量学指标,-1表示找不到
+                cc=j[0:j.find('cc')]  #绝对体积数
+                totalv=GetAbsoluteVolume(total_dose,ROI['name']);
+                tmpdose = total_dose.GetDoseAtRelativeVolumes(RoiName = ROI['name'], RelativeVolumes = [float(cc)/totalv])
+                d[j]=tmpdose[0]
+            elif cmp(j,'Volume')==0:
+                d[j]=GetAbsoluteVolume(total_dose,ROI['name'])
+    return d
 #---------------------------------------------------------------------------------#
-def main(plans,targets,serialOARs,parallelOARs,prescription):
-    patient=get_current('Patient')
+def main(patients,plans,targets,OARs):
+    if not patients:  #patients不为空
+        patient=get_current('Patient')
     #检查对象是否存在
-    planlist=[]
-    for plan in patient.TreatmentPlans:
-        planlist.append(plan.Name)
+        planlist=[]
+        for plan in patient.TreatmentPlans:
+            planlist.append(plan.Name)
     
-    examination=get_current('Examination')
-    structure_set=patient.PatientModel.StructureSets[examination.Name]
-    roilist=[]
-    for rg in structure_set.RoiGeometries:
-        roilist.append(rg.OfRoi.Name)
+        examination=get_current('Examination')
+        structure_set=patient.PatientModel.StructureSets[examination.Name]
+        roilist=[]
+        for rg in structure_set.RoiGeometries:
+            roilist.append(rg.OfRoi.Name)
     #同时遍历和删除，推荐用倒序    
-    check(planlist,plans)
-    check(roilist,targets)
-    check(roilist,serialOARs)
-    check(roilist,parallelOARs)
-            
+        check(planlist,plans)
+        check(roilist,targets)
+        check(roilist,OARs)
+        #寻找外轮廓
+        try:
+            external_roi = next(r for r in patient.PatientModel.RegionsOfInterest if r.Type == 'External')
+        except:
+            raise Exception('No external ROI defined')
+        extername=external_roi.Name
     #统计
-    total_outcome=[]
-    for plan in plans:
-        total_dose=patient.TreatmentPlans[plan].TreatmentCourse.TotalDose
-        outcome=[]
-        for PTV in targets:
-            d=OrderedDict()
-            d['roiname']=PTV
-            Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = PTV, RelativeVolumes = [0.02,0.5,0.95,0.98])
-            d['D2']=Doses[0]
-            d['D50']=Doses[1]
-            d['D95']=Doses[2]
-            d['D98']=Doses[3]
-            d['HI']=(Doses[0]-Doses[3])/Doses[1]
-            d['CI']=GetConformationIndex(total_dose, prescription, PTV)
-            outcome.append(d)
+        total_outcome=[]
+        for plan in plans:
+            total_dose=patient.TreatmentPlans[plan].TreatmentCourse.TotalDose
+            outcome=[]
+            for PTV in targets:
+                outcome.append(getdoseindicators(total_dose,PTV,extername))
         
-        for OAR in serialOARs:
-            d=OrderedDict()
-            d['roiname']=OAR
-            Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.02,0.5])
-            d['D2']=Doses[0]
-            d['D50']=Doses[1]
-            outcome.append(d)
-            
-        for OAR in parallelOARs:
-            d=OrderedDict()
-            d['roiname']=OAR
-            if OAR in ['Parotid L','Parotid R','Liver']:
-                Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.5])
-                d['D50']=Doses[0]
-                Volumes= total_dose.GetRelativeVolumeAtDoseValues(RoiName = OAR,DoseValues=[3000])
-                d['V30']=Volumes[0]
-            elif OAR=='Thyroid':
-                Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.5])
-                d['D50']=Doses[0]
-                Volumes= total_dose.GetRelativeVolumeAtDoseValues(RoiName = OAR,DoseValues=[4000,5000])
-                d['V40']=Volumes[0]
-                d['V50']=Volumes[1]
-            elif OAR in ['Lung All','Lung R','Lung L']:
-                Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.5])
-                d['D50']=Doses[0]
-                d['DAve'] = total_dose.GetDoseStatistic(RoiName = OAR, DoseType='Average')
-                Volumes= total_dose.GetRelativeVolumeAtDoseValues(RoiName = OAR,DoseValues=[500,2000])
-                d['V5']=Volumes[0]
-                d['V20']=Volumes[1]
-            elif OAR=='Heart':
-                Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.5])
-                d['D50']=Doses[0]
-                Volumes= total_dose.GetRelativeVolumeAtDoseValues(RoiName = OAR,DoseValues=[3000,4000])
-                d['V30']=Volumes[0]
-                d['V40']=Volumes[1]
-            elif OAR in ['Humerus Head L','Humerus Head R','Bladder','Rectum','Colon','Femoral head R','Femoral head L','Small Intestine']:
-                Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.02,0.5])
-                d['D2']=Doses[0]
-                d['D50']=Doses[1]
-                Volumes= total_dose.GetRelativeVolumeAtDoseValues(RoiName = OAR,DoseValues=[5000])
-                d['V50']=Volumes[0]
-            elif OAR in ['Kidney R','Kidney L']:
-                Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.5])
-                d['D50']=Doses[0]
-                Volumes= total_dose.GetRelativeVolumeAtDoseValues(RoiName = OAR,DoseValues=[2000])
-                d['V20']=Volumes[0]
-            elif OAR=='Stomach':
-                Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.02,0.5])
-                d['D2']=Doses[0]
-                d['D50']=Doses[1]
-                Volumes= total_dose.GetRelativeVolumeAtDoseValues(RoiName = OAR,DoseValues=[3000])
-                d['V30']=Volumes[0]
-            else:
-                Doses = total_dose.GetDoseAtRelativeVolumes(RoiName = OAR, RelativeVolumes = [0.02,0.5])
-                d['D2']=Doses[0]
-                d['D50']=Doses[1]
-            outcome.append(d)
+            for OAR in OARs:
+                outcome.append(getdoseindicators(total_dose,OAR,extername))
+          
         total_outcome.append(outcome)
-    exporttoexcel(patient.PatientName,total_outcome,plans)
+        exporttoexcel(patient.PatientName,total_outcome,plans)
 #--------------------------------------------------------------------------------------------------------------#
 if __name__=='__main__':
-    main(plans=['MCO-test5','Plan_1'],targets=['PTV\''],serialOARs=['Brain Stem','Brain Stem PRV','Spinal Cord','Spinal Cord PRV','Lens L','Lens R'],
-      parallelOARs=['Optic Chiasm','Optic Nerver L','Optic Nerve R','Temporal Lobe L','Temporal Lobe R','Parotid L','Parotid R','Larynx','Ear L','Ear R','Piturary',
-      'Mandible L','Mandible R','TMJ L','TMJ R','Eye L','Eye R','Lung All','Lung R','Lung L','Heart','Esophagus','Humerus Head L','Humerus Head R','Liver','Stomach',
-      'Small Intestine','Kidney R','Kidney L','Bladder','Rectum','Colon','Femoral head R','Femoral head L','Ovaries','Testicles'],prescription=3000)
+    main(patients=[],plans=['Copy'],targets=[{'name':'APGTVnx','Ds':[2,50,95,98],'Vs':[],'special':['CI','HI','Volume'],'prescription':7400},
+    {'name':'APGTVnd','Ds':[2,50,95,98],'Vs':[],'special':['CI','HI','Volume'],'prescription':7000},
+    {'name':'APCTV1','Ds':[2,50,95,98],'Vs':[],'special':['CI','HI','Volume'],'prescription':6000},
+    {'name':'APCTV2+APCTVnd','Ds':[2,50,95,98],'Vs':[],'special':['CI','HI','Volume'],'prescription':5400}],
+    OARs=[{'name':'Brain Stem+3 PRV','Ds':[50],'Vs':[10,15,20,25,30,35,40,45,50,54,55,60],'special':['max']},
+    {'name':'Spinal Cord+5 PRV','Ds':[1,5,10,50],'Vs':[50],'special':['max']},
+    {'name':'Temporal Lobe L','Ds':[],'Vs':[60,65,70],'special':['max','mean','0.5cc','1cc']},
+    {'name':'Temporal Lobe L PRV','Ds':[],'Vs':[],'special':['max','mean']},
+    {'name':'Temporal Lobe R','Ds':[],'Vs':[60,65,70],'special':['max','mean','0.5cc','1cc']},
+    {'name':'Temporal Lobe R PRV','Ds':[],'Vs':[],'special':['max','mean']},
+    {'name':'Optic Chiasm PRV','Ds':[1,5,10],'Vs':[],'special':['max','mean']},
+    {'name':'Optic Nerver L PRV','Ds':[1,5,10],'Vs':[],'special':['max','mean']},
+    {'name':'Optic Nerve R PRV','Ds':[1,5,10],'Vs':[],'special':['max','mean']},
+    {'name':'Ear L','Ds':[],'Vs':[],'special':['max','mean']},
+    {'name':'Ear R','Ds':[],'Vs':[],'special':['max','mean']},
+    {'name':'Parotid gland L','Ds':[],'Vs':[30],'special':['mean']},
+    {'name':'Parotid gland R','Ds':[],'Vs':[30],'special':['mean']}])
